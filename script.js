96107006b9f7d9d07660756e48f313a5e6b6dbbe
hunrayy <henryokiyi8@gmail.com>
2025-01-19 21:06:04 +0100

    added new function

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ for ( let i = 12; i > 0; i--) {
     let option = `<option value="${i}">${i}<option>`;
     selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option)
 }
+function hello(){
+    
+}
 
 for (let i = 59; i >= 0; i--) {
     i = i < 10 ? "0" + i : i;
