e4bafc454d1b38a9b0fa985a45d159eb295ae5fb
hunrayy <henryokiyi8@gmail.com>
2025-03-21 21:46:04 +0100

    removed from script

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ setAlarmBtn = document.querySelector("#setAlarm");
 let alarmTime; isAlarmSet = false,
 ringtone = new Audio("./files/fantasy.mp3")
 
-
-console.log('hello')
 for ( let i = 12; i > 0; i--) {
     i = i < 10 ? "0" + i : i;
     let option = `<option value="${i}">${i}<option>`;
